6a81ede20d9d203697a3e4eed9a35ee36473343f
Sam Kane <samuelkane526@gmail.com>
2024-12-31 20:15:40 -0600

    Added actual extension

---
+++ content.js
@@ -0,0 +1,37 @@
+var jsonFileUrl = "https://brawny-melodic-slayer.glitch.me/script.js" //ADD JSON FILE URL HERE
+
+var getIns = async () => {
+  try {
+    let response = await fetch(jsonFileUrl);
+    let data = await response.text();
+    return data;
+  } catch (error) {
+    console.error('Error fetching the txt file:', error);
+  }
+};
+
+var getChildEl = (element, childPath) => { 
+  childPath.forEach((index) => { 
+    element = element.children[index]; 
+  }); 
+  return element; 
+};
+
+var main = async () => {  
+  var ins = await getIns();
+  var ins = JSON.parse(ins)
+  ins.instructions.forEach(item => 
+    {
+    var element = document.querySelector(item.selector)
+    if(item.childPath) element = getChildEl(element,item.childPath)
+    if(item.action == "txt") element.textContent = item.value;
+    if(item.action == "append"){
+      let parent = element.parentNode;
+      element.parentNode.removeChild(element)
+      let newElement = document.createElement(item.elementType)
+      newElement.innerHTML = item.value;
+      parent.appendChild(newElement)
+    }
+  });
+};
+main();
